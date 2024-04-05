8e5ff47fca3257a3e0f3be01d6949710caa76f45
advaith <advaithj1@gmail.com>
2024-04-04 20:15:11 -0700

    piazza-poster

---
+++ README.md
@@ -0,0 +1,30 @@
+# piazza-poster
+
+piazza-poster crossposts all posts and announcements from Piazza into Discord, with a button to view the original post in Piazza. Announcements are posted to an announcements channel, and other posts are posted to a feed channel.
+
+Also see [ed-poster](https://github.com/advaith1/ed-poster) and [canvas-poster](https://github.com/advaith1/canvas-poster).
+
+## Prerequisites
+
+piazza-poster runs on Cloudflare Workers for free.
+
+You will need:
+* A [Cloudflare account](https://dash.cloudflare.com/sign-up)
+* [Node.js](https://nodejs.org/en) installed locally
+* A Discord server, where you have Manage Webhook permissions
+
+## Setup
+
+First, clone this repository and run `pnpm i`. (If pnpm isn't installed, run `corepack enable` first.) Copy wrangler.example.toml to wrangler.toml and courses.example.json to courses.json.
+
+Run `pnpm set-email` and type in your Piazza account email when prompted. You may be prompted to log in to your Cloudflare account first. Then, run `pnpm set-password` and type in your Piazza account password.
+
+Run `pnpm create-kv`, then copy the provided binding ID into wrangler.toml.
+
+In courses.json, set the `courseID` to a unique string for the course, and set `piazzaID` to the generated course ID in the Piazza URL.
+
+Use [Webhook Creator](https://webhook-creator.advaith.workers.dev) to create a webhook in the announcements channel and get the URL. (Don't create the webhook in Discord settings, use this tool instead.) Copy the URL it gives you and set it as `announcementWebhook` in courses.json.
+
+Do the same for the feed channel, setting the second webhook's `url` as `feedWebhook`.
+
+When you're done, run `pnpm run deploy`. piazza-poster will now check every minute for new Piazza posts and send them to the appropriate Discord channel.
---
+++ src/index.ts
@@ -0,0 +1,182 @@
+export interface Env {
+	PIAZZA_EMAIL: string
+	PIAZZA_PASSWORD: string
+
+	LAST_POSTS: KVNamespace
+}
+
+interface PiazzaFeedResponse {
+	result: {
+		feed: {
+			nr: number
+			type: 'note' | 'question'
+			subject: string
+			content_snipet: string
+			folders: string[]
+			tags: string[]
+		}[]
+	}
+}
+
+interface PiazzaPostResponse {
+	result: {
+		history: {
+			uid?: string
+			subject: string
+			content: string
+		}[]
+	}
+}
+
+interface User {
+	name: string
+}
+
+interface PiazzaUserResponse {
+	result: User[]
+}
+
+interface Course {
+	courseID: string
+	piazzaID: string
+	announcementWebhook: string
+	feedWebhook: string
+}
+
+import he from 'he'
+
+import courses from '../courses.json'
+
+const capitalize = (str: string) => str[0].toUpperCase() + str.slice(1)
+
+const trim = (text: string, max: number) => text.length > max ? text.substring(0, max - 1)+'…' : text
+
+const checkCourse = async ({ courseID, piazzaID, announcementWebhook, feedWebhook }: Course, cookie: string, csrfToken: string, env: Env) => {
+	const { result: { feed } } = await (await fetch('https://piazza.com/logic/api?method=network.get_my_feed', {
+		method: 'POST',
+		headers: {
+			cookie,
+			'csrf-token': csrfToken,
+			'content-type': 'application/json',
+		},
+		body: JSON.stringify({
+			method: 'network.get_my_feed',
+			params: {
+				nid: piazzaID,
+				offset: 0,
+				limit: 200
+			}
+		})
+	})).json() as PiazzaFeedResponse
+
+	const lastPostNumber = +(await env.LAST_POSTS.get(courseID) ?? 0)
+
+	const newPosts = feed.filter(post => post.nr > lastPostNumber)
+	if (!newPosts.length) return new Response('')
+
+	await env.LAST_POSTS.put(courseID, Math.max(...feed.map(post => post.nr)).toString())
+
+	newPosts.sort((a, b) => a.nr - b.nr)
+
+	for (const post of newPosts) {
+		const { result: { history: [postData] } } = await (await fetch('https://piazza.com/logic/api?method=content.get', {
+			method: 'POST',
+			headers: {
+				cookie,
+				'csrf-token': csrfToken,
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify({
+				method: 'content.get',
+				params: {
+					cid: post.nr,
+					nid: piazzaID
+				}
+			})
+		})).json() as PiazzaPostResponse
+
+		let user: User | undefined
+		if (postData.uid) {
+			const { result: [userData] } = await (await fetch('https://piazza.com/logic/api?method=network.get_users', {
+				method: 'POST',
+				headers: {
+					cookie,
+					'csrf-token': csrfToken,
+					'content-type': 'application/json',
+				},
+				body: JSON.stringify({
+					method: 'network.get_users',
+					params: {
+						ids: [postData.uid],
+						nid: piazzaID
+					}
+				})
+			})).json() as PiazzaUserResponse
+			user = userData
+		}
+
+		const title = he.decode(postData.subject)
+
+		const content = (await (await fetch('https://turndown.advaith.io', {
+			method: 'POST',
+			body: postData.content
+		})).text()).replace(/https?:\/\/[^\s"'<>]+/g, url => url.replaceAll('\\', '')).replace(/\[(.+?)\]\(\1\)/g, '$1').replace(/!\[.*?\]\(\//g, '[[image]](https://piazza.com/')
+
+		const webhook = post.tags.includes('instructor-note') ? announcementWebhook : feedWebhook
+		await fetch(webhook, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				username: `${user?.name ?? 'Anonymous'} on Piazza`,
+				content: trim(`## ${title}\n${capitalize(post.type)} ${post.folders.length ? `in ${post.folders.join(', ')}` : ''}\n\n${content}`, 2000),
+				components: [
+					{
+						type: 1,
+						components: [
+							{
+								type: 2,
+								label: 'View on Piazza',
+								style: 5,
+								url: `https://piazza.com/class/${piazzaID}/post/${post.nr}`
+							}
+						]
+					}
+				],
+				allowed_mentions: {
+					parse: []
+				}
+			})
+		})
+	}
+}
+
+const getCookies = (res: Response) => res.headers.getSetCookie().map(cookie => cookie.split(';')[0]).join('; ')
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		return new Response('ok')
+	},
+	async scheduled(controller: ScheduledController, env: Env, ctx: ExecutionContext): Promise<void> {
+		const csrfRes = await fetch('https://piazza.com/main/csrf_token')
+		const csrfToken = (await csrfRes.text()).split('"')[1]
+
+		const loginBody = new FormData()
+		loginBody.append('from', '/signup')
+		loginBody.append('email', env.PIAZZA_EMAIL)
+		loginBody.append('password', env.PIAZZA_PASSWORD)
+		loginBody.append('remember', 'on')
+		loginBody.append('csrf_token', csrfToken)
+		const loginRes = await fetch('https://piazza.com/class', {
+			method: 'POST',
+			headers: {
+				cookie: getCookies(csrfRes)
+			},
+			body: loginBody
+		})
+		const cookie = getCookies(loginRes)
+
+		for (const course of courses) {
+			await checkCourse(course, cookie, csrfToken, env)
+		}
+	}
+}
