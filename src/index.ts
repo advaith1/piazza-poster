19ca8b5b21bd7a448478893152eb2c79b2139461
advaith <advaithj1@gmail.com>
2026-02-01 22:18:48 -0800

    use components v2 for images

--- src/index.ts
+++ src/index.ts
@@ -82,15 +82,48 @@ const checkCourse = async ({ courseID, piazzaID, announcementWebhook, feedWebhoo
 
 		const title = he.decode(postData.subject)
 
+		const header = `## ${title}\n${capitalize(post.type)} ${post.folders.length ? `in ${post.folders.join(', ')}` : ''}`
+
 		const content = (
 			postData.content.startsWith('<md>') && postData.content.endsWith('</md>')
 				? postData.content.slice(4, -5)
 				: turndownService.turndown(createDocument(postData.content))
 		)
 			.replace(/https?:\/\/[^\s"'<>]+/g, url => url.replaceAll('\\', ''))
-			.replace(/\[(.+?)\]\(\1\)/g, '$1')
-			.replace(/!\[.*?\]\(\//g, '[[image]](https://piazza.com/')
+			.replace(/\[(.+)\]\(\1\)/g, '$1')
+			.replace(/!\[.+\]\(\/(.+)\)/g, '<__block_boundary>__image:https://piazza.com/$1<__block_boundary>')
 			.replaceAll('](/', '](https://piazza.com/')
+
+		// if the post contains images, send a components v2 message with media gallery components
+		const components = []
+		let isComponentsV2 = false
+		if (content.includes('<__block_boundary>')) {
+			isComponentsV2 = true
+			components.push({
+				type: 10,
+				content: trim(header, 1000)
+			})
+
+			for (const block of content.split('<__block_boundary>').filter(part => part.trim())) {
+				if (block.startsWith('__image:')) {
+					components.push({
+						type: 12,
+						items: [
+							{
+								media: {
+									url: block.slice(8),
+								}
+							}
+						]
+					})
+				} else {
+					components.push({
+						type: 10,
+						content: trim(block, 2000)
+					})
+				}
+			}
+		}
 
 		const webhook = post.tags.includes('instructor-note') ? announcementWebhook : feedWebhook
 		await fetch(webhook + '?with_components=true', {
@@ -98,8 +131,9 @@ const checkCourse = async ({ courseID, piazzaID, announcementWebhook, feedWebhoo
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({
 				username: `${user?.name ?? 'Anonymous'} on Piazza`,
-				content: trim(`## ${title}\n${capitalize(post.type)} ${post.folders.length ? `in ${post.folders.join(', ')}` : ''}\n\n${content}`, 2000),
+				content: isComponentsV2 ? undefined : trim(`${header}\n\n${content}`, 2000),
 				components: [
+					...components,
 					{
 						type: 1,
 						components: [
@@ -114,7 +148,8 @@ const checkCourse = async ({ courseID, piazzaID, announcementWebhook, feedWebhoo
 				],
 				allowed_mentions: {
 					parse: []
-				}
+				},
+				flags: isComponentsV2 ? 1 << 15 : 0
 			})
 		})
 	}
